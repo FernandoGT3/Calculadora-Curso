1f1c8068636f7e446eef03f4d9ec8121b3d73908
Luiz Moloni <luiz.moloni@gmail.com>
2022-03-07 14:19:56 -0300

    Funções-Js
    Criando as Funções: pressEnter(para que o cálculo seja realizado ao apertamos a tecla enter),  makeAccount(que realiza a conta),  clearDisplay(que reseta o display da calculadora) e deleteOne(que apaga um elemento).

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,18 @@
     function createCalculator(){
         return {
             display: document.querySelector('.display'),
-            
 
             start(){
-                this.clickButton()
+                this.clickButton();
+                this.pressEnter();
+            },
+
+            pressEnter(){
+                this.display.addEventListener('keyup', (event) =>{
+                    if(event.keyCode === 13){
+                        this.makeAccount(); //o this continua sendo a calculadora por ser uma ArrowFunction
+                    }
+                });
             },
 
             clickButton(){
@@ -16,12 +24,54 @@
                     if(element.classList.contains('btn-num')){
                         this.btnForDisplay(element.innerText);
                     }
+
+                    if(element.classList.contains('btn-clear')){
+                        this.clearDisplay();
+                    }
+
+                    if(element.classList.contains('btn-del')){
+                        this.deleteOne();
+                    }
+
+                    if(element.classList.contains('btn-equal')){
+                        this.makeAccount();
+                    }
+
                 }.bind(this)); //fazendo com que o this aponte para a calculadora
                 //(.bind) é um método de funções
+                //Se usarmos ArrowFunction o this será cravado em quem criou o elemento
+                //Ou seja, não mudam o comportamento do this
             },
 
-            btnForDisplay(valor){
-                this.display.value += valor;
+            makeAccount(){
+                let count = this.display.value;
+                try{
+                    count = eval(count);
+
+                    if(!count || typeof(count) !== 'number'){
+                        throw TypeError;
+                        return;
+                    }
+
+                    this.display.value = count;
+                }catch(error){
+                    alert("Conta Inválida");
+                    this.clearDisplay();
+                    return;
+                }
+            },
+
+            clearDisplay(){
+                this.display.value = " ";
+            },
+
+            deleteOne(){
+                this.display.value = this.display.value.slice(0,-1);
+                //console.log(this.display.value);
+            },
+
+            btnForDisplay(content){
+                this.display.value += content;
             }
         };
     }
